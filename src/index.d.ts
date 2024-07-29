f4da835293ef6ed8bf83e59a9977a0858308dd95
Ashutosh Tewari <krishwave66@gmail.com>
2024-07-30 01:00:30 +0530

    Provided named imports

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,13 @@
 declare module "@expensio/sharedlib" {
-	export * from "./errors/ApplicationError";
-	export * from "./errors/AuthorizationError";
-	export * from "./errors/DatabaseError";
-	export * from "./errors/NotFoundError";
-	export * from "./errors/ValidationError";
-	export * from "./errors/otp/OtpSendingError";
-	export * from "./errors/otp/RateLimitError";
-	export * from "./errors/AuthenticationError";
-	export * from "./errors/InternalServerError";
-	export * from "./middlewares/errorHandlingMiddleware";
-	export * from "./middlewares/authMiddleware";
+	export { default as ApplicationError } from "./errors/ApplicationError.js";
+	export { default as AuthorizationError } from "./errors/AuthorizationError.js";
+	export { default as DatabaseError } from "./errors/DatabaseError.js";
+	export { default as NotFoundError } from "./errors/NotFoundError.js";
+	export { default as ValidationError } from "./errors/ValidationError.js";
+	export { default as OtpSendingError } from "./errors/otp/OtpSendingError.js";
+	export { default as RateLimitError } from "./errors/otp/RateLimitError.js";
+	export { default as AuthenticationError } from "./errors/AuthenticationError.js";
+	export { default as InternalServerError } from "./errors/InternalServerError.js";
+	export { default as errorHandlingMiddleware } from "./middlewares/errorHandlingMiddleware.js";
+	export { default as authMiddleware } from "./middlewares/authMiddleware.js";
 }
