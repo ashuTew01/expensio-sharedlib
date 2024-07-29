9e2d20b25fa35c1244f32a589adf31efae2ea668
Ashutosh Tewari <krishwave66@gmail.com>
2024-07-30 00:44:12 +0530

    change type to module

---
+++ scripts/commit.js
@@ -0,0 +1,31 @@
+// scripts/commit.js
+import { execSync } from "child_process";
+
+const args = process.argv.slice(2);
+const versionType = args[0] || "patch";
+const commitMessage = args[1] || `Bump version to ${versionType}`;
+
+try {
+	execSync("git add .", { stdio: "inherit" });
+
+	execSync(`git commit -m "${commitMessage}"`, { stdio: "inherit" });
+
+	execSync(`npm version ${versionType}`, { stdio: "inherit" });
+
+	execSync("npm publish", { stdio: "inherit" });
+
+	console.log(`Successfully published a ${versionType} version.`);
+} catch (error) {
+	console.error("Error during version bump and publish process:", error);
+	process.exit(1);
+}
+
+// Usage
+// Patch Version Update:
+// npm run pub:patch "Your commit message for patch"
+
+// Minor Version Update:
+// npm run pub:minor "Your commit message for minor update"
+
+// Major Version Update:
+// npm run pub:major "Your commit message for major update"
