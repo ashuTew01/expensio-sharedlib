30fd42593912cd1b9383e31075ed1497403eece0
ashuTew01 <krishwave66@gmail.com>
2024-08-13 10:27:34 +0530

    bug fix

--- src/config/rabbitmq.js
+++ src/config/rabbitmq.js
@@ -1,8 +1,19 @@
 import amqp from "amqplib";
 
+let channel;
+
 const connectRabbitMQ = async () => {
-	const connection = await amqp.connect(process.env.RABBITMQ_URL);
-	return connection;
+	if (!channel) {
+		try {
+			const connection = await amqp.connect(process.env.RABBITMQ_URL);
+			channel = await connection.createChannel();
+			console.log("Connected to RabbitMQ and channel created");
+		} catch (error) {
+			console.error("Failed to connect to RabbitMQ:", error);
+			throw error;
+		}
+	}
+	return channel;
 };
 
-export default connectRabbitMQ;
+export { connectRabbitMQ, channel };
