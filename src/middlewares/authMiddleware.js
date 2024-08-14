55017625f9615ba74ea44e5c1d1117cd4398e9b1
ashuTew01 <krishwave66@gmail.com>
2024-08-14 12:18:43 +0530

    bug fixes

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-	const token = req.header("Authorization").replace("Bearer ", "");
+	const token = req.header("Authorization")?.replace("Bearer ", "");
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 		req.user = decoded;
