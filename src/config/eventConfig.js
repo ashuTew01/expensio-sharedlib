d115b5fbdfc2132664c63b49cd8d4f468b684648
ashuTew01 <krishwave66@gmail.com>
2024-08-19 15:11:04 +0530

    add EXPENSE_CREATED event

--- src/config/eventConfig.js
+++ src/config/eventConfig.js
@@ -2,6 +2,8 @@ export const EVENTS = {
 	USER_DELETED: "USER_DELETED",
 	USER_DELETION_FAILED: "USER_DELETION_FAILED",
 	USER_DELETION_UNDONE: "USER_DELETION_UNDONE",
+
+	EXPENSE_CREATED: "EXPENSE_CREATED",
 };
 
 export const EXCHANGES = {
@@ -14,5 +16,7 @@ export const ROUTING_KEYS = {
 	USER_DELETED: "user.deleted",
 	USER_DELETION_FAILED: "user.deletion.failed",
 	USER_DELETION_UNDONE: "user.deletion.undone",
+
+	EXPENSE_CREATED: "expense.created",
 	// add new routing keys here
 };
