35c171607a0359ee778d63caad219ac35bcb948d
ashuTew01 <krishwave66@gmail.com>
2024-08-29 14:45:10 +0530

    improve eventConfig for easy adding of new events. add some new events

--- src/config/eventConfig.js
+++ src/config/eventConfig.js
@@ -1,49 +1,37 @@
-/**
- * @typedef {Object} Events
- * @property {string} USER_DELETED
- * @property {string} USER_DELETION_FAILED
- * @property {string} USER_DELETION_UNDONE
- * @property {string} EXPENSE_CREATED
- * @property {string} EXPENSE_DELETED
- */
+const eventNames = [
+	//user events
+	"USER_DELETED",
+	"USER_DELETION_FAILED",
+	"USER_DELETION_UNDONE",
 
-/** @type {Events} */
-export const EVENTS = {
-	USER_DELETED: "USER_DELETED",
-	USER_DELETION_FAILED: "USER_DELETION_FAILED",
-	USER_DELETION_UNDONE: "USER_DELETION_UNDONE",
-	EXPENSE_CREATED: "EXPENSE_CREATED",
-	EXPENSE_DELETED: "EXPENSE_DELETED",
-};
+	//expense events
+	"EXPENSE_CREATED",
+	"EXPENSE_DELETED",
 
-/**
- * @typedef {Object} Exchanges
- * @property {string} USER
- * @property {string} EXPENSE
- */
+	//income events
+	"INCOME_CREATED",
+	"INCOME_DELETED",
 
-/** @type {Exchanges} */
+	//financial data events
+	"FINANCIALDATA_UPDATED_EXPENSE",
+	"FINANCIALDATA_UPDATED_INCOME",
+];
+
+export const EVENTS = eventNames.reduce((acc, eventName) => {
+	acc[eventName] = eventName;
+	return acc;
+}, {});
+
 export const EXCHANGES = {
 	USER: "user-events",
 	EXPENSE: "expense-events",
+	FINANCIALDATA: "financial-data-events",
+	INCOME: "income-events",
 	// add new exchanges here
 };
 
-/**
- * @typedef {Object} RoutingKeys
- * @property {string} USER_DELETED
- * @property {string} USER_DELETION_FAILED
- * @property {string} USER_DELETION_UNDONE
- * @property {string} EXPENSE_CREATED
- * @property {string} EXPENSE_DELETED
- */
-
-/** @type {RoutingKeys} */
-export const ROUTING_KEYS = {
-	USER_DELETED: "user.deleted",
-	USER_DELETION_FAILED: "user.deletion.failed",
-	USER_DELETION_UNDONE: "user.deletion.undone",
-	EXPENSE_CREATED: "expense.created",
-	EXPENSE_DELETED: "expense.deleted",
-	// add new routing keys here
-};
+export const ROUTING_KEYS = eventNames.reduce((keys, event) => {
+	const routingKey = event.toLowerCase().replace(/_/g, ".");
+	keys[event] = routingKey;
+	return keys;
+}, {});
