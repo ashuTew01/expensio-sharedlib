56cf8f10c4f17f0634eda85f16177fb99e5bd9f2
ashuTew01 <krishwave66@gmail.com>
2024-09-16 14:56:49 +0530

    test patch 0.2

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -2,45 +2,46 @@ import { logError, logWarning, logInfo } from "../config/logger.js";
 
 const errorHandlingMiddleware = (err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
-	const publicMessage =
-		err.publicMessage || err.message || "An internal server error occurred";
+	res.status(statusCode).json({ error: "An internal server error occurred" });
+	// const publicMessage =
+	// 	err.publicMessage || err.message || "An internal server error occurred";
 
-	if (statusCode >= 500) {
-		logError(
-			`${statusCode} - ${err.name}: ${err.message}`
-			// 	{
-			// 	url: req.originalUrl,
-			// 	body: req.body,
-			// 	method: req.method,
-			// 	ip: req.ip,
-			// 	errorStack: err.stack,
-			// }
-		);
-	} else if (statusCode >= 400) {
-		logWarning(
-			`${statusCode} - ${err.name}: ${err.message}`
-			// 	{
-			// 	url: req.originalUrl,
-			// 	body: req.body,
-			// 	method: req.method,
-			// 	ip: req.ip,
-			// 	errorStack: err.stack,
-			// }
-		);
-	} else {
-		logInfo(
-			`${statusCode} - ${err.name}: ${err.message}`
-			// 	{
-			// 	url: req.originalUrl,
-			// 	body: req.body,
-			// 	method: req.method,
-			// 	ip: req.ip,
-			// 	errorStack: err.stack,
-			// }
-		);
-	}
+	// if (statusCode >= 500) {
+	// 	logError(
+	// 		`${statusCode} - ${err.name}: ${err.message}`
+	// 		// 	{
+	// 		// 	url: req.originalUrl,
+	// 		// 	body: req.body,
+	// 		// 	method: req.method,
+	// 		// 	ip: req.ip,
+	// 		// 	errorStack: err.stack,
+	// 		// }
+	// 	);
+	// } else if (statusCode >= 400) {
+	// 	logWarning(
+	// 		`${statusCode} - ${err.name}: ${err.message}`
+	// 		// 	{
+	// 		// 	url: req.originalUrl,
+	// 		// 	body: req.body,
+	// 		// 	method: req.method,
+	// 		// 	ip: req.ip,
+	// 		// 	errorStack: err.stack,
+	// 		// }
+	// 	);
+	// } else {
+	// 	logInfo(
+	// 		`${statusCode} - ${err.name}: ${err.message}`
+	// 		// 	{
+	// 		// 	url: req.originalUrl,
+	// 		// 	body: req.body,
+	// 		// 	method: req.method,
+	// 		// 	ip: req.ip,
+	// 		// 	errorStack: err.stack,
+	// 		// }
+	// 	);
+	// }
 
-	res.status(statusCode).json({ error: publicMessage });
+	// res.status(statusCode).json({ error: publicMessage });
 };
 
 export default errorHandlingMiddleware;
